45e84afe897e35b24d259e7e066f55ed8bd385c3
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-01-19 10:18:52 +0700

    Webp to PNG Converter

---
+++ Webp to PNG Converter/main.py
@@ -0,0 +1,15 @@
+import glob
+import os
+from webptools import dwebp  # library to convert webp to png
+os.chdir("./")  # working directory
+# scans directory for webp files
+webp_list = glob.glob("*.webp")
+
+# using for loop to go through the files in the list
+for filename in webp_list:
+    # removes the .webp extension and maintain the original filename for output
+    outname = filename[:-4] + "png"
+    dwebp(input_image=filename, output_image=outname, option="-o", logging="-v")
+
+# single input single output
+#print(dwebp(input_image=filename, output_image=outname, option="-o", logging="-v"))
