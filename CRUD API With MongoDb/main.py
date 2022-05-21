c2c7f3635938393d15482d5a466838d0c329c9b5
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-05-21 13:36:09 +0700

    CRUD API With MongoDb

--- CRUD API With MongoDb/main.py
+++ CRUD API With MongoDb/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes.student import student_router
+import uvicorn
+import os
 
 app = FastAPI()
 # uvicorn main:app --reload
 
 # Register routes
 app.include_router(student_router)
+
+if __name__ == '__main__':
+    host = "127.0.0.1"
+    port = 5000
+
+    os.system("start \"\" http://" + host + ":" + str(port) + "/docs")
+    uvicorn.run("main:app", host=host, port=port, reload=True)
