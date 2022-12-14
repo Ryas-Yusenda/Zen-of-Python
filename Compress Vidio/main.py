9440b4a33ff9668312a63cc315c678ca68caacdb
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-12-15 04:33:46 +0700

    Compress Vidio

---
+++ Compress Vidio/main.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+
+src = './'
+dst = './output'
+
+for root, dirs, filenames in os.walk(src, topdown=False):
+    #print(filenames)
+    for filename in filenames:
+        print('[INFO] 1',filename)
+        try:
+            _format = ''
+            if ".flv" in filename.lower():
+                _format=".flv"
+            if ".mp4" in filename.lower():
+                _format=".mp4"
+            if ".avi" in filename.lower():
+                _format=".avi"
+            if ".mov" in filename.lower():
+                _format=".mov"
+
+            inputfile = os.path.join(root, filename)
+            print('[INFO] 1',inputfile)
+            outputfile = os.path.join(dst, filename.lower().replace(_format, ".mp4"))
+            subprocess.call(['ffmpeg', '-i', inputfile, '-crf', '35', outputfile])  
+        except:
+            print("An exception occurred")
