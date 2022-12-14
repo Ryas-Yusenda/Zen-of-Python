084b4babf1708087010536faa21f43a7e2dd4d54
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-12-15 04:33:34 +0700

    Build 15 Applications and Games

---
+++ Build 15 Applications and Games/Project-1_Dice_rolling_stimulator.py
@@ -0,0 +1,26 @@
+import sys
+from PySide import QtGui
+
+"""
+Jago Ngoding tutorial PySide berbahasa Indonesia
+
+author: Ibnu Jakaria
+"""
+
+
+class Jendela(QtGui.QWidget):
+
+  def __init__(self):
+    super(Jendela, self).__init__()
+
+    self.siapkanUI()
+
+  def siapkanUI(self):
+    self.resize(800, 600)
+    self.setWindowTitle('Jago Ngoding')
+
+
+app = QtGui.QApplication(sys.argv)
+jendela = Jendela()
+jendela.show()
+sys.exit(app.exec_())
