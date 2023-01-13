28a20579907e8819154471d7b552bdc088a5650c
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2023-01-13 21:48:01 +0700

    Flask Form

---
+++ Flask Form/main.py
@@ -0,0 +1,40 @@
+# create server with python flask
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+from flask_mail import Mail, Message
+import MySQLdb.cursors
+import re
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length
+import os
+import secrets
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)
